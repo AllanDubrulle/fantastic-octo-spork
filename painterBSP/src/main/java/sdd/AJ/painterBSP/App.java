57ce89b6d93db5e6f97294ce24e1bf0a1d7c5607
allan <allandu1@live.fr>
2019-02-04 11:24:43 +0100

    initialisation maven project

---
+++ painterBSP/src/main/java/sdd/AJ/painterBSP/App.java
@@ -0,0 +1,13 @@
+package sdd.AJ.painterBSP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
