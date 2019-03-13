cccedc2ed1e2b14252952c3823a52bdc8fd46518
allan <allandu1@live.fr>
2019-03-13 15:08:36 +0100

    painter algorithm and functional interface Equation

--- painterBSP/src/main/java/sdd/AJ/painterBSP/BSPLIb/BSPTree.java
+++ painterBSP/src/main/java/sdd/AJ/painterBSP/BSPLIb/BSPTree.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import sdd.AJ.painterBSP.BSPLIb.Heuristic.Heuristic;
+import sdd.AJ.painterBSP.util.Equation;
 import sdd.AJ.painterBSP.util.Segment;
 
 public class BSPTree { //TODO Eliminer les methodes inutilisees
 	private List<Segment> list;
 	private BSPTree left, right;
-	
+	private Equation equation;
 //constructeurs
-	public BSPTree(List<Segment> list, BSPTree l, BSPTree r) {
+	public BSPTree(List<Segment> list, BSPTree l, BSPTree r,Equation equation) {
 		this.list = list;
 		left = l;
 		right = r;
+		this.setEquation(equation);
 	}
 	public BSPTree() {
-		this(null,null,null);
+		this(null,null,null,null);
 	}
 	
 	public BSPTree(List<Segment> list, Heuristic h) { //TODO Implémenter algo travail preparatif
@@ -46,7 +48,13 @@ public class BSPTree { //TODO Eliminer les methodes inutilisees
 		right = r;
 	}
 
-//test de l'arbre vide, c'est-a-dire du "noeud vide"
+public Equation getEquation() {
+		return equation;
+	}
+	public void setEquation(Equation equation) {
+		this.equation = equation;
+	}
+	//test de l'arbre vide, c'est-a-dire du "noeud vide"
 	public boolean isEmpty() {
 		if (list == null && left == null && right == null) 
 			return true;
@@ -55,7 +63,7 @@ public class BSPTree { //TODO Eliminer les methodes inutilisees
 	}
 	
 //remplit un noeud vide avec la donnee d et 2 sous-arbres vides
-	public void insertEmpty(Segment s) {
+	private void insertEmpty(Segment s) {
 		list = new ArrayList<Segment>(); //TODO Choisir le type final de liste
 		list.add(s);
 		left = new BSPTree();
@@ -63,9 +71,17 @@ public class BSPTree { //TODO Eliminer les methodes inutilisees
 	}
 	
 	public void insertSegment(Segment s) {
-		list.add(s);
+		if (this.isEmpty()){
+			this.insertEmpty(s);
+		}
+		else
+			list.add(s);
 	}
 	
+	public boolean isLeaf()
+	{
+		return left == null && right == null; 
+	}
 	
 	
 //calcul de la hauteur
---
+++ painterBSP/src/main/java/sdd/AJ/painterBSP/util/Equation.java
@@ -0,0 +1,6 @@
+package sdd.AJ.painterBSP.util;
+
+public interface Equation
+{
+	public double solve (double x, double y);
+}
