a2a19bf26ed7e924f06d39b29f4c7fa0531453e7
James Main <james.alexander.main@gmail.com>
2019-03-24 01:29:58 +0100

    Added construction of BSP. Addition of multiple methods for Segment Main constructor of BSPTree class implemented. The Segment class has also seen multiple additions, one of which is a method to split the segment into two parts, when given a line equation, so that each half lies on an opposite side of the plane. There also has been the addition of a method giving, from a segment, the equation of the line with the same direction but passing by (0, 0).

--- painterBSP/src/main/java/sdd/AJ/painterBSP/BSPLIb/BSPTree.java
+++ painterBSP/src/main/java/sdd/AJ/painterBSP/BSPLIb/BSPTree.java
@@ -1,18 +1,88 @@
 package sdd.AJ.painterBSP.BSPLIb;
-
 import java.util.ArrayList;
 import java.util.List;
-
+import java.lang.Math;
 import sdd.AJ.painterBSP.BSPLIb.Heuristic.Heuristic;
+import sdd.AJ.painterBSP.BSPLIb.Heuristic.LinearHeuristic;
 import sdd.AJ.painterBSP.util.Equation;
 import sdd.AJ.painterBSP.util.Segment;
+import java.util.Collections;
 
 public class BSPTree { //TODO Eliminer les methodes inutilisees
     private List<Segment> list;
     private BSPTree left, right;
     private Equation equation;
-    //constructeurs
 
+
+    //constructeurs
+    public BSPTree(List<Segment> list, Heuristic heuristic) {
+        //Algorithme de création du BSP
+        // Si la liste est vide, on crée un arbre
+        this.list = new ArrayList();
+        left = null;
+        right = null;
+        equation = null;
+        if(!list.isEmpty()) {
+            Segment pivot = heuristic.selectSegment(list);
+            equation = pivot.lineEquation();
+            List<Segment> leftList = new ArrayList<>();
+            List<Segment> rightList = new ArrayList<>();
+            double d = equation.solve(pivot.x1, pivot.x2);
+            double eps =10e-9;
+            for (Segment s: list) {
+                if (Math.abs(equation.solve(s.x1, s.x2) - d) < eps) {
+                    //On teste si le segment est inclus dans la droite
+                    if (Math.abs(equation.solve(s.y1, s.y2) - d) < eps) {
+                        this.list.add(s);
+                    }
+                    //teste si le segment est inclus dans le plan positif
+                    else if (equation.solve(s.y1, s.y2) > d) {
+                        rightList.add(s);
+                    }
+                    else {
+                        leftList.add(s);
+                    }
+                }
+
+                else if (Math.abs(equation.solve(s.y1, s.y2) - d)< eps) {
+                    if (equation.solve(s.x1, s.x2) > d) {
+                        rightList.add(s);
+                    }
+                    else {
+                        leftList.add(s);
+                    }
+                }
+
+                else if (equation.solve(s.x1, s.x2) > d) {
+                    if (equation.solve(s.y1, s.y2) > d) {
+                        rightList.add(s);
+                    }
+                    else {
+                        Segment[] brokenS = s.breakSegment(equation, d);
+                        rightList.add(brokenS[0]);
+                        leftList.add(brokenS[1]);
+                    }
+                }
+
+                else if (equation.solve(s.x1, s.x2) < d) {
+                    if (equation.solve(s.y1, s.y2) < d) {
+                        leftList.add(s);
+                    }
+
+                    else {
+                        Segment[] brokenS = s.breakSegment(equation, d);
+                        rightList.add(brokenS[1]);
+                        leftList.add(brokenS[0]);
+                    }
+                }
+            }
+        }
+    }
+
+    public static BSPTree RandomBSPTree(List<Segment> list) {
+         Collections.shuffle(list);
+         return new BSPTree(list, new LinearHeuristic());
+    }
 
     public BSPTree(List<Segment> list, BSPTree l, BSPTree r,Equation equation) {
         this.list = list;
@@ -96,5 +166,4 @@ public class BSPTree { //TODO Eliminer les methodes inutilisees
         else
             return 1+left.size()+right.size();
     }
-
 }
