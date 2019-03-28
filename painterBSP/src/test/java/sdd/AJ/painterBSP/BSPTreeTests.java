4eb756391f09f47095c3d5beb7c171940134489a
allan <allandu1@live.fr>
2019-03-28 18:37:51 +0100

    test unitaire BSPTree

---
+++ painterBSP/src/test/java/sdd/AJ/painterBSP/BSPTreeTests.java
@@ -0,0 +1,26 @@
+package sdd.AJ.painterBSP;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import sdd.AJ.painterBSP.BSPLib.BSPTree;
+import sdd.AJ.painterBSP.util.IllustrationInputReader;
+import sdd.AJ.painterBSP.util.Segment;
+public class BSPTreeTests
+{
+    public static ArrayList<Segment> segmentList; 
+    @BeforeClass
+    public static void initializeFile() throws IOException
+    {
+        IllustrationInputReader iir = new IllustrationInputReader("Scenes/random/randomHuge.txt");
+        segmentList = (ArrayList<Segment>) iir.getSegments();
+    }
+    
+    @Test
+    public void stackoverflow()
+    {
+         BSPTree test = BSPTree.RandomBSPTree(segmentList);
+    }
+}
