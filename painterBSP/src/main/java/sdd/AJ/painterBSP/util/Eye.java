87216373db1441ce36fc64f18ef96c61f060e485
James Main <james.alexander.main@gmail.com>
2019-03-30 17:28:15 +0100

    Implementation of eye using scalar product

--- painterBSP/src/main/java/sdd/AJ/painterBSP/util/Eye.java
+++ painterBSP/src/main/java/sdd/AJ/painterBSP/util/Eye.java
@@ -5,14 +5,25 @@ import sdd.AJ.painterBSP.BSPLib.Painter;
 import java.lang.Math;
 import java.util.List;
 
-
+    /* Represents an eye located at given coordinates,
+     * and in a specified direction.
+     */
 public class Eye
 {
+    public static final int COVERS = 1;
+    public static final int INTERSECTS = 0;
+    public static final int OUT_OF_VIEW = -1;
 
     private double x;
     private double y;
     private double angle;
 
+    /**
+     * Class constructor.
+     * @param x      x-coordinate of the eye
+     * @param y      y-coordinate of the eye
+     * @param angle  angle representing the forward direction
+     */
     public Eye(double x, double y, double angle)
     {
         this.x = x;
@@ -20,6 +31,12 @@ public class Eye
         this.angle = angle;
     }
 
+    /**
+     * Updates all fields of the eye with given parameters.
+     * @param x      x-coordinate of the eye
+     * @param y      y-coordinate of the eye
+     * @param angle  angle representing the forward direction
+     */
     public void update(double x, double y, double angle)
     {
         this.x = x;
@@ -27,7 +44,9 @@ public class Eye
         this.angle = angle;
     }
 
-
+    /**
+     * Resets all parameters to 0 (both coordinates and angle).
+     */
     public void resetPosition()
     {
         //resets values to 0
@@ -82,56 +101,171 @@ public class Eye
 
     public void draw(Segment s, Painter p)
     {
-        double dx1 = s.x1 - x;
-        double dx2 = s.x2 - y;
-        double angleX = rotateIfNecessary(Math.atan2(dx1, dx2));
-        double dy1 = s.y1 - x;
-        double dy2 = s.y2 - y;
-        double angleY = rotateIfNecessary(Math.atan2(dy1, dy2));
-        if (angleX < angleY)
+
+    }
+
+    /**
+     * Computes for a visible segment how much of the eye span
+     * it covers (by giving the percentage to be filled in).
+     * @param u1 the first point's x coordinate
+     * @param v1 the first point's y coordinate
+     * @param u2 the second point's x coordinate
+     * @param v2 the second point's y coordinate
+     * @return an array of two double containing the start
+     *         and end such that [start, end] is the subset of
+     *         [0, 1] to be filled in.
+     */
+    private double[] getVisibleProportion(double u1, double v1, double u2, double v2)
+    {
+        double start, end;
+        double a = Math.cos(angle);
+        double b = Math.sin(angle);
+        // We center everything to simplify calculations.
+        u1 = u1 - this.x;
+        v1 = v1 - this.y;
+        u2 = u2 - this.x;
+        v2 = v2 - this.y;
+        double temp1 = (a * u1 + b * v1)/Math.hypot(u1, v1);
+        double temp2 = (a * u2 + b * v2)/ Math.hypot(u2, v2);
+        // temp_i is cos(phi_i) where phi_i is the angle between
+        // (a, b) and (ui, vi) for i = 1, 2
+        temp1 = Math.acos(temp1);
+        temp2 = Math.acos(temp2);
+        // now temp_i is the unsigned phi, to sign it we use the fact
+        // that being to the right => negative angle
+        temp1 = isToTheRight(u1, v1) ? -temp1:  temp1;
+        temp2 = isToTheRight(u2, v2) ? -temp2 : temp2;
+
+        // We now consider the bijection f(x) = (x + pi/4) / (pi/2)
+        // f: [-pi/4, pi/4] -> [0, 1] plus the cases when tempi
+        // exceeds the considered interval
+
+        if (temp1 > Math.PI/4)
         {
-            double temp = angleX;
-            angleX = angleY;
-            angleY = temp;
+            end = 1;
+            start = (temp2 + Math.PI/4) * (2 / Math.PI);
         }
-        // Ici  on suppose que angle X plus grand que angle Y
-        if (isVisible(angleX, angleY))
+        else if (temp2 > Math.PI/4)
         {
-            double[] visibleProp = getVisibleProportion(angleX, angleY);
-            p.draw(visibleProp[0], visibleProp[1], s.getColor());
+            end = 1;
+            start = (temp1 + Math.PI/4) * (2 / Math.PI);
         }
-    }
+        if (temp1 < -Math.PI/4)
+        {
+            start = 1;
+            end = (temp2 + Math.PI/4) * (2 / Math.PI);
+        }
+        else if (temp2 < -Math.PI/4)
+        {
+            start = 1;
+            end = (temp1 + Math.PI/4) * (2 / Math.PI);
+        }
 
-    private double[] getVisibleProportion(double angleX, double angleY)
-    {
-        if (angleX > this.angle + Math.PI/2)
-            angleX = this.angle + Math.PI/2;
-        if (angleY < this.angle)
-            angleY = this.angle;
-        //Ici on considere la bij entre [0, 1] et [angle, angle + pi/2]
-        double start = (angleY - this.angle) * (2/Math.PI);
-        double end = (angleX - this.angle) * (2/Math.PI);;
-        return new double[] {start, end};
-    }
-
-    private boolean isVisible(double angle1, double angle2)
-    {
-        // Hypothese: deja rotationne si c'etait necessaire
-        boolean ok1 = this.angle < angle1
-            && angle1 < this.angle + Math.PI/2;
-        boolean ok2 = this.angle < angle2
-            && angle2 < this.angle + Math.PI/2;
-        // cas ou aucun point n'est dans le cone mais le segment est visible
-        boolean ok3 = angle1 > this.angle + Math.PI/2  && angle2 < this.angle; // cas casse
-        return ok1 || ok2 || ok3;
-    }
-
-    private double rotateIfNecessary(double angleToRotate)
-    {
-        if (this.angle > Math.PI/2 && angleToRotate < -Math.PI/2)
-            return angleToRotate + 2* Math.PI;
         else
-            return angleToRotate;
+        {
+            if (temp1 > temp2)
+            {
+                start = (temp2 + Math.PI/4) * (2 / Math.PI);
+                end = (temp1 + Math.PI/4) * (2 / Math.PI);
+            }
+            else
+            {
+                end = (temp2 + Math.PI/4) * (2 / Math.PI);
+                start = (temp1 + Math.PI/4) * (2 / Math.PI);
+            }
+        }
+        return new double[] { start, end };
+    }
+
+    /**
+     * Determines whether a segment is visible or not.
+     * @param u1 the first point's x coordinate
+     * @param v1 the first point's y coordinate
+     * @param u2 the second point's x coordinate
+     * @param v2 the second point's y coordinate
+     * @return Eye.COVERS if the segment covers the whole
+     * viewpoint, Eye.INTERSECTS if the segment is partially
+     * visible or Eye.OUT_OF_VIEW if it is completely invisible.
+     */
+    public int isVisible(double u1, double v1, double u2, double v2)
+    {
+        double a = Math.cos(angle);
+        double b = Math.sin(angle);
+        // We center everything to simplify calculations.
+        u1 = u1 - this.x;
+        v1 = v1 - this.y;
+
+        u2 = u2 - this.x;
+        v2 = v2 - this.y;
+        // We calculate the scalar product between the parameters and (a, b)
+        double scal1 = a * u1 + b * v1;
+        double scal2 = a * u2 + b * v2;
+
+        // To test whether a point is visible, we use the scalar
+        // product. A point (u, v) is visible iff its angular coordinate
+        // if at most pi/4 from angle iff scalar product
+        // between (a, b) and (u, v)/||(u, v)|| is at least sqrt(2)/2
+
+        if ( scal1 >= Math.sqrt(2)/(2 * Math.hypot(u1, v1)))
+            return INTERSECTS;
+        else if ( scal2 >= Math.sqrt(2)/(2 * Math.hypot(u2, v2)))
+            return INTERSECTS;
+        // As the view angle is a convex subset of the plane,
+        // if both points lie on a same side (left or right) of
+        // the eye, then the segment is not visible.
+        // Moreover, the segment cannot be seen if both points
+        // lie beneath the eye.
+        if ((isToTheRight(u1, v1) && isToTheRight(u2, v2)) ||
+            (!isToTheRight(u1, v1) && !isToTheRight(u2, v2)) ||
+            (scal1 <= 0 && scal2 <= 0))
+            return OUT_OF_VIEW;
+        // We can now assume that both points lie on a different side
+        // of the plane and that at least one is "above" the eye (relative
+        // to angle).
+
+        // Assuming this, the view is completely obstructed if and only if
+        // (0, 0) is beneath the line defined by the points (u1, v1) and
+        // (u2, v2).
+
+        // Assume the point lying above (0, 0) relatively to angle
+        // is to the right of the eye, thus the other one is to the left
+        // (and may lie beneath the eye).
+
+        if (isToTheRight(u1, v1))
+        {
+            if (aboveOrigin(u2, v2, u1, v1))
+                return COVERS;
+            else
+                return OUT_OF_VIEW;
+        }
+        else if (isToTheRight(u2, v2))
+        {
+            if (aboveOrigin(u1, v1, u2, v2))
+                return COVERS;
+        }
+
+        return OUT_OF_VIEW;
+    }
+
+
+
+    /**
+     * Returns true iff the point (u, v) is to the right of (0, 0),
+     * relatively to the direction given by angle.
+     */
+    private boolean isToTheRight(double u, double v)
+    {
+        return - Math.sin(angle) * u + Math.cos(angle) * v >= 0;
+    }
+
+    /**
+     * Returns true iff (0, 0) is above the line relatively to the normal
+     * vector ((v1 - v2), (u2 - u1)) (that is the vector (u2-u1, v2 -v1)
+     * rotated 90 degrees to the left).
+     */
+    private boolean aboveOrigin(double u1, double v1, double u2, double v2)
+    {
+        return (v1 - v2) * u1 + (u2 - u1) * v1 >= 0;
     }
 
 }
